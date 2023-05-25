19515aef25009eb381bf654b76ac8f7a86005acb
Chandan Debnath <35869474+chandanadittyo@users.noreply.github.com>
2023-05-25 13:18:58 +0600

    use context added

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,26 +1,25 @@
 // eslint-disable-next-line no-unused-vars
+import { useContext } from "react";
 import ThemeContext from "./Context/ThemeContext";
 import Counter from "./Counter";
 import HoverCounter from "./HoverCounter";
 
 // eslint-disable-next-line react/prop-types
 export default function Content() {
+  const context = useContext(ThemeContext);
+  const { theme, switchTheme } = context;
   return (
     <div>
       <h2>This is Content</h2>
 
       <Counter>
         {(counter, incrementCount) => (
-          <ThemeContext.Consumer>
-            {({ theme, switchTheme }) => (
-              <HoverCounter
-                count={counter}
-                incrementCount={incrementCount}
-                theme={theme}
-                switchTheme={switchTheme}
-              />
-            )}
-          </ThemeContext.Consumer>
+          <HoverCounter
+            count={counter}
+            incrementCount={incrementCount}
+            theme={theme}
+            switchTheme={switchTheme}
+          />
         )}
       </Counter>
     </div>
